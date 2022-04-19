efaeb52e2d95629417a441231ef56ff1073186b6
Googly-Boogly <mainalextaylor@gmail.com>
2022-04-19 16:09:32 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+print('Hellow World')
+name = 'alex'
+print(f'Hello {name}')
+numb = 5
+print('Hello', numb)
+print('Hello' + str(numb))
+food_one = 'pizza'
+food_two = 'ice cream'
+print(f'I love to eat {food_one} and {food_two}')
+print('I love to eat ' + str(food_one) +  ' and ' + str(food_two))
